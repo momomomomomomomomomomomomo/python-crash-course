9bebba59729d9c9b4a6b4b9e28bcc34e82a9a48e
momomomomomomomomomomomomo <142402844+momomomomomomomomomomomomo@users.noreply.github.com>
2026-03-02 18:01:27 +0200

    chapter5

---
+++ chapter5/ordinal-numbers.py
@@ -0,0 +1,9 @@
+for numm in range(1, 10):
+    if numm == 1:
+        print(f"{numm}st")
+    elif numm == 2:
+        print(f"{numm}nd")
+    elif numm == 3:
+        print(f"{numm}rd")
+    else:
+        print(f"{numm}th")
---
+++ chapter5/users.py
@@ -0,0 +1,8 @@
+
+users = ["maria", "john", "susan", "admin", "alice"]
+for user in users:    
+    if user == "admin":
+        print("Hello admin, would you like to see a status report?")    
+    else:
+        print(f"Hello {user}, thank you for logging in again.")
+
